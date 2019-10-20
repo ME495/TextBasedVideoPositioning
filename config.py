7b54c74fa24bad26a901a87430a1e489a08237ae
ME495 <ME495@qq.com>
2019-10-20 15:14:05 +0800

    Add layers/ and config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+
+class Config(object):
+    pass
+
+def get_config():
+    config = Config()
+    
+    config.data_path = '/data02/chengjian19/dataset/Charades/'
+    config.rgb_path = '/data02/chengjian19/dataset/Charades/Charades_v1_rgb/'
+    config.flow_path = '/data02/chengjian19/dataset/Charades/Charades_v1_flow/'
+    config.train_file = '/data02/chengjian19/dataset/Charades/charades_sta_train.txt'
+    config.test_file = '/data02/chengjian19/dataset/Charades/charades_sta_test.txt'
+    
+    config.video_feature_num = None
+    config.video_feature_dim = None
+    config.sentence_feature_dim = None
+    
