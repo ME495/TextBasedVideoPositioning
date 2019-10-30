bb689b06fa642bfaf635c7f6cc9c398d772c24c2
ME495 <ME495@qq.com>
2019-10-30 13:43:09 +0800

    Add raw_video_path and video480_path to config.py.

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ def get_config():
     config = Config()
     
     config.data_path = '/data02/chengjian19/dataset/Charades/'
+    config.raw_video_path = '/data02/chengjian19/dataset/Charades/Charades_v1/'
+    config.video480_path ='/data02/chengjian19/dataset/Charades/Charades_v1_480/'
     config.rgb_path = '/data02/chengjian19/dataset/Charades/Charades_v1_rgb/'
     config.flow_path = '/data02/chengjian19/dataset/Charades/Charades_v1_flow/'
     config.train_file = '/data02/chengjian19/dataset/Charades/charades_sta_train.txt'
